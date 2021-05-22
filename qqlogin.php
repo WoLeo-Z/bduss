b7087a48e24cee8790beea6f7b648b3028855637
WoL <45914900+WoLeo-Z@users.noreply.github.com>
2021-05-22 09:52:22 +0800

    Add files via upload

---
+++ qqlogin.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+
+require 'qqlogin.class.php';
+$login=new qq_login();
+if($_GET['do']=='qrlogin'){
+	$login->qrlogin();
+}
+if($_GET['do']=='getqrpic'){
+	$login->getqrpic();
+}
+if($_GET['do']=='getwxpic'){
+	$array = $login->getwxpic();
+}
+if($_GET['do']=='wxlogin'){
+	$array = $login->wxlogin();
+}
+echo json_encode($array);
